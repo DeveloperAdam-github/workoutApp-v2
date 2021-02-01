26bd3906c4ae99ac8c339d1ad9a5242e230c60b0
Adam Powell <developeradam@adams-air.home>
2021-02-01 17:00:33 +0000

    working on exercise form submission

--- src/pages/NewWorkout.js
+++ src/pages/NewWorkout.js
@@ -1,13 +1,68 @@
-import { Button } from '@material-ui/core';
+import { Button, Checkbox } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import RotateLeftIcon from '@material-ui/icons/RotateLeft';
 import './NewWorkout.css';
-import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import { v4 as uuidv4 } from 'uuid';
+import ClearIcon from '@material-ui/icons/Clear';
+import { db } from '../firebase';
+import firebase from 'firebase';
 
 const NewWorkout = () => {
   const [time, setTime] = useState(0);
   const [timerOn, setTimerOn] = useState(false);
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const [inputFields, setInputFields] = useState([
+    {
+      id: uuidv4(),
+      exercise: '',
+      weight: '',
+      reps: '',
+    },
+  ]);
+
+  const addExercise = () => {
+    setInputFields([
+      ...inputFields,
+      { id: uuidv4(), exercise: '', weight: '', reps: '' },
+    ]);
+  };
+
+  const deleteExercise = (id) => {
+    const values = [...inputFields];
+    values.splice(
+      values.findIndex((value) => value.id === id),
+      1
+    );
+    setInputFields(values);
+  };
+
+  const handleChangeInput = (id, e) => {
+    const newInputFields = inputFields.map((i) => {
+      if (id === i.id) {
+        i[e.target.name] = e.target.value;
+      }
+      return i;
+    });
+
+    setInputFields(newInputFields);
+  };
+
+  const finishWorkout = () => {
+    setTimerOn(false);
+  };
+
+  const onSubmit = (formData) => {
+    console.log(formData);
+    db.collection('users').add({
+      exercise: formData.exercise,
+      weight: formData.weight,
+      reps: formData.reps,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  };
 
   useEffect(() => {
     let interval = null;
@@ -27,14 +82,15 @@ const NewWorkout = () => {
     setTimerOn(!timerOn);
   }
 
-  const finishWorkout = () => {
-    setTimerOn(false);
-  };
+  // const finishWorkout = () => {
+  //   onSubmit();
+  //   setTimerOn(false);
+  // };
 
   return (
     <div className='newWorkout'>
       <div className='newWorkout__container'>
-        <form action=''>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className='newWorkout__top'>
             <Button
               onClick={toggle}
@@ -51,6 +107,7 @@ const NewWorkout = () => {
               {!timerOn ? <p>Start</p> : <p>Pause</p>}{' '}
             </Button>
             <Button
+              type='submit'
               onClick={finishWorkout}
               style={{
                 backgroundColor: '#27AA61',
@@ -66,8 +123,14 @@ const NewWorkout = () => {
           </div>
           <div className='newWorkout__middle'>
             <p className='newWorkout__title'>
-              <input type='text' placeholder='New Workout..' />
+              <input
+                name='newWorkout'
+                type='text'
+                placeholder='New Workout..'
+                ref={register({ required: true })}
+              />
             </p>
+
             <p className='newWorkout__timer'>
               <span>{('0' + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
               <span>{('0' + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
@@ -86,9 +149,68 @@ const NewWorkout = () => {
               }}
             />
           </div>
-          <div className='newWorkout__exercises'></div>
+          <div className='newWorkout__exercisesTitle'>
+            <p style={{ position: 'relative', right: '2%' }}>Set</p>
+            <p style={{ position: 'relative', right: '3%' }}>Exercise</p>
+            <p>KG</p>
+            <p>Reps</p>
+            <p></p>
+            <p></p>
+          </div>
+
+          {inputFields.map((inputField) => (
+            <div className='newWorkout__exercises' key={inputField.id}>
+              <div className='newWorkout__set'>
+                <p>1</p>
+              </div>
+              <div className='newWorkout__exercise'>
+                <input
+                  value={inputField.exercise}
+                  name='exercise'
+                  onChange={(e) => handleChangeInput(inputField.id, e)}
+                  type='text'
+                  placeholder='Bench Press'
+                  ref={register({ required: true })}
+                />
+              </div>
+              <div className='newWorkout__weight'>
+                <input
+                  value={inputField.weight}
+                  name='weight'
+                  onChange={(e) => handleChangeInput(inputField.id, e)}
+                  type='text'
+                  placeholder='100'
+                  ref={register({ required: true })}
+                />
+              </div>
+              <div className='newWorkout__reps'>
+                <input
+                  value={inputField.reps}
+                  name='reps'
+                  onChange={(e) => handleChangeInput(inputField.id, e)}
+                  type='text'
+                  placeholder='10'
+                  ref={register({ required: true })}
+                />
+              </div>
+              <Checkbox className='newWorkout__checkbox' />
+              <Button
+                disabled={inputFields.length === 1}
+                onClick={() => deleteExercise(inputField.id)}
+                style={{
+                  minWidth: '10px',
+                  width: '16px',
+                  height: '16px',
+                  backgroundColor: '#FEEDEF',
+                }}
+              >
+                <ClearIcon className='newWorkout__clear' />
+              </Button>
+            </div>
+          ))}
           <div className='newWorkout__footer'>
             <Button
+              onClick={addExercise}
               style={{
                 backgroundColor: '#EBF4FF',
                 marginBottom: '25px',
